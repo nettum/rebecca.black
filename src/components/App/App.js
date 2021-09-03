ee869f7fc4b367d902650810ecbf51d617044503
Marius Nettum <nettum@gmail.com>
2021-09-03 17:20:04 +0200

    WIP styling and some css animation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,8 @@ const App = () => {
   const [ seenVideos, setSeenVideos ] = useState([]);
   const [ currentVideo, setCurrentVideo ] = useState(null);
 
-  const dayNumber = new Date().getDay();
-  const dayData = data.find(day => day.day === dayNumber);
+  const dayNumber = 1; //new Date().getDay();
+  const dayData = data.find(day => day.daynumber === dayNumber);
 
   const getRandomArrayElement = (arr) => arr[Math.floor(Math.random()*arr.length)];
 
@@ -38,15 +38,16 @@ const App = () => {
       <header>
         <h1>Is it friday yet?</h1>
       </header>
-      <main>
-        <p>{dayData.text}</p>
-        <p>{currentVideo.artist} - {currentVideo.song}</p>
-        {dayNumber !== 5 && <button onClick={changeSong}>Ny sang!</button>}
+      <main className={`day${dayNumber}`}>
+        <h2 dangerouslySetInnerHTML={{ __html: dayData.text }} />
+        {dayNumber !== 5 && <p>But fear not, here is a musicvideo about {dayData.dayname}s</p>}
+        <h3>{currentVideo.artist} - {currentVideo.song}</h3>
+        {dayNumber !== 5 && <button onClick={changeSong}>I don't like this song,<br />gimme another one!</button>}
         <div className="youtube-wrapper">
           <iframe
             width="560"
             height="315"
-            src={`https://www.youtube.com/embed/${currentVideo.youtube_id}`}
+            src={`https://www.youtube.com/embed/${currentVideo.youtubeid}`}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
